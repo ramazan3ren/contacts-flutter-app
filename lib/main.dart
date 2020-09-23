ac499b15d97fdbf7cf31043f050227519a28b023
Burak Demirpolat <o.b.demirpolat@gmail.com>
2020-09-23 21:23:31 +0300

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_contact/base_contacts.dart';
 import 'package:flutter_contact/contact.dart';
 import 'package:flutter_contact/contacts.dart';
 import 'package:permission_handler/permission_handler.dart';
@@ -20,58 +19,135 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
+class Row {
+  Contact contact;
+  bool checked;
+  Row(this.contact, this.checked);
+}
+
 class _HomePageState extends State<HomePage> {
-  List<Contact> contacts = [];
-  
+  bool permissionState = false;
+  //Rehber kayıtları bu listede yer alacak.
+  //Liste içinde bir adet "contact" bir de "bool" değer var.
+  //Her bir contactın kendine ait bool değeri var. Bu bool değerler ile işaretli/işaretsiz durumunu uygulayacağız.
+  List<Row> contacts = [];
 
   @override
   void initState() {
     super.initState();
-    _getAllContacts();
-    
+    _permission();
+  }
+
+  _permission() async {
+    var permission = await Permission.contacts.status;
+    //Eğer kullanıcı izin vermiş ise kayıtları alıyoruz ve metodu bitiriyoruz.
+    if (permission.isGranted) {
+      setState(() {
+        this.permissionState = true;
+      });
+      _getAllContacts();
+      return;
+    }
+    //Eğer kullanıcıya daha önce hiç izin sorulmadıysa
+    //Veya kullanıcı kalıcı olarak reddetmediyse izin istiyoruz.
+    if (permission.isUndetermined || !permission.isPermanentlyDenied) {
+      await Permission.contacts.request();
+      permission = await Permission.contacts.status;
+      if (permission.isGranted) {
+        setState(() {
+          this.permissionState = true;
+        });
+        _getAllContacts();
+        return;
+      }
+    }
+  }
+
+  //Eğer kullanıcı ayarlara gidip rehber iznini tekrar verir ise
+  //_permission metodu tekrar çalışacak ve yetki verilmiş mi kontrol edecek.
+  _openSettingsForPermission() async {
+    openAppSettings();
+    _permission();
   }
 
   _getAllContacts() async {
-    List<Contact> _contacts = await (Contacts.streamContacts()).toList();
-    setState(() {
-      contacts = _contacts;
-    });
+    if (this.permissionState) {
+      await (Contacts.streamContacts()).forEach((element) {
+        setState(() {
+          this.contacts.add(new Row(element, false));
+        });
+      });
+    }
+  }
+
+  //Eğer kullanıcı rehbere erişim izni vermemiş ise bu ekran açılacak.
+  Widget _notPermitted() {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text("Rehberinizi görüntülememiz için izninize ihtiyacımız var."),
+          RaisedButton(
+            child: Text(
+              "Ayarları Aç",
+            ),
+            onPressed: _openSettingsForPermission,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _body() {
+    if (!this.permissionState) {
+      return _notPermitted();
+    }
+    return ListView.builder(
+      itemCount: contacts.length,
+      itemBuilder: (context, index) {
+        Row row = contacts[index];
+        return _contactCard(row);
+      },
+    );
+  }
+
+  Widget _contactCard(Row row) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 10, right: 10),
+      child: Card(
+        margin: EdgeInsets.all(10),
+        elevation: 10,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ListTile(
+            title: Text(
+              row.contact.displayName,
+              style: TextStyle(color: Colors.black87),
+            ),
+            leading: Icon(
+              Icons.account_circle,
+              size: 45,
+              color: Colors.black87,
+            ),
+            trailing: Checkbox(
+              value: row.checked,
+              onChanged: (incomingValue) {
+                setState(() {
+                  row.checked = incomingValue;
+                });
+              },
+            ),
+          ),
+        ),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-          itemCount: contacts.length,
-          itemBuilder: (context, index) {
-            Contact contact = contacts[index];
-            return Padding(
-              padding: const EdgeInsets.only(left: 10, right: 10),
-              child: Card(
-                margin: EdgeInsets.all(10),
-                elevation: 10,
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: ListTile(
-                    
-                    title: Text(
-                      contact.displayName,
-                      style: TextStyle(color: Colors.black87),
-                    ),
-                    leading: Icon(
-                      Icons.account_circle,
-                      size: 45,
-                      color: Colors.black87,
-                    ),
-                    trailing: Checkbox(
-                      value: false,
-                      onChanged: (incomingValue) {},
-                    ),
-                  ),
-                ),
-              ),
-            );
-          }),
+      body: _body(),
     );
   }
 }
